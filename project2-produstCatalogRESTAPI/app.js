0db827d7318675def537fc68e53c4008c9420b82
John <johnprogrammer2005@gmail.com>
2018-01-17 14:21:42 +0800

    project2 completed

--- project2-produstCatalogRESTAPI/app.js
+++ project2-produstCatalogRESTAPI/app.js
@@ -8,6 +8,14 @@ const app = express();
 const port = 3000;
 app.use(bodyParser.json());
 
+// make this api public
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+
 // home
 app.get('/', (req, res, next) => {
     res.send('please use /api/products/');
@@ -40,17 +48,41 @@ app.get('/api/products/:id', (req, res, next) => {
 
 // add  product
 app.post('/api/products', (req, res, next) => {
-    res.send('add product ');
+     db.products.insert(req.body, (err, doc) => {
+         if (err) {
+             res.end(err);
+         }
+         console.log('adding product..');
+         res.json(doc);
+     });
 });
 
 // update  product
 app.put('/api/products/:id', (req, res, next) => {
-    res.send('update product ' + req.params.id);
+    db.products.findAndModify({query: {_id: mongojs.ObjectId(req.params.id)},
+    update: {
+        $set: {
+            name: req.body.name,
+            category: req.body.category,
+            details: req.body.details
+        }}, new: true}, (err, doc) => {
+            if (err) {
+                res.send(err);
+            }
+            console.log('Updating product...');
+            res.json(doc);
+        })
 });
 
 // delete  product
 app.delete('/api/products/:id', (req, res, next) => {
-    res.send('delete product ' + req.params.id);
+    db.products.remove({_id: mongojs.ObjectId(req.params.id)}, (err, doc)=> {
+        if (err) {
+            res.send(err);
+        }
+        console.log('Removing Product...');
+        res.json(doc);
+    })
 });
 
 
